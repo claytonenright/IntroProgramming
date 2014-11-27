b21ab2de57037905cd3020f9f75a3b704c99ab7c
Clayton Enright <clayton.enright@gmail.com>
2014-11-27 12:05:29 -0500

    Updates for Project 5
    Added new buttons to allow users to click the "take" and "inventory"
    commands, created global arrays for location instances and item
    instances, added more items to the game and updated the map to show
    their locations

--- Game Project/GameJava_ClaytonEnright.js
+++ Game Project/GameJava_ClaytonEnright.js
@@ -1,8 +1,11 @@
    // Global Variables
 var playerLocation = 0;
 var counter = 0;
+var inventory = [];
 var hasBacon = false;
 var hasHam = false;
+var hasSilverware = false;
+var hasPig = false;
 
    // Load Function         
 function init() {
@@ -16,9 +19,13 @@ function addScore(points) {
    counter = counter + points;
    score.value = counter;
 }
+
+
    
    //  Location Specific Functions   
 function checkLocation() {
+   dispMsg(locales[playerLocation].desc);
+   availButton();
    switch(playerLocation) {
       case 0: visit0();   break;
       case 1: visit1();   break;
@@ -31,8 +38,10 @@ function checkLocation() {
       case 8: visit8();   break;
       case 9: visit9();   break;
       default: errorLoc();
-   }  
+   }
+
 }
+
 
 function availButton() {
    switch(playerLocation) {
@@ -96,7 +105,7 @@ function dispMsg(msg) {
 }
 
 function helpMsg() {
-   dispMsg("Type in \'n, s, e, or w\' to move. Type in \'take\' to acquire an item. Type in \'inv\' to check your inventory.");
+   dispMsg("Use the buttons below! Or, type in \'n, s, e, or w\' to move. Type in \'take\' to acquire an item. Type in \'inv\' to check your inventory.");
 }
          
 function errorLoc() {
@@ -201,18 +210,60 @@ function moveEast() {
     checkLocation();
 }
 
+   // Item Functions
+function Item() {
+   this.id = "";
+   this.name = "";
+   this.desc = "";
+   this.toString = function() {return "[" + this.id + ", " + this.name + ", " + "\"" + this.desc + "\"" + "]";}
+}
+
+var Bacon = new Item();
+Bacon.id = 1;
+Bacon.name = "Bacon";
+Bacon.desc = "A large portion of bacon.";
+
+var Ham = new Item();
+Ham.id = 2;
+Ham.name = "Ham";
+Ham.desc = "A chunk of ham.";
+
+var Pig = new Item();
+Pig.id = 3;
+Pig.name = "Pig";
+Pig.desc = "A friendly swine.";
+
+var Silverware = new Item();
+Silverware.id = 4;
+Silverware.name = "Silverware";
+Silverware.desc = "A fork and a knife.";
+
 function takeItem() {
    if (playerLocation === 7 && !hasBacon) {
       hasBacon = true;
+      inventory.push(Bacon);
       dispMsg("You put some of the bacon in your pockets.");
       addScore(5);
    } else if (playerLocation === 8 && !hasHam) {
         hasHam = true;
+        inventory.push(Ham);
         dispMsg("You pull off a hearty piece of the honey ham.");
         addScore(5);
-     } else {dispMsg("There are no items here!");}
+     } else if (playerLocation === 2 && !hasSilverware) {
+          hasSilverware = true;
+          inventory.push(Silverware);
+          dispMsg("You put a fork and a knife in your pockets. Just in case.");
+          addScore(5);
+       } else if (playerLocation === 5 && !hasPig) {
+            hasPig = true;
+            inventory.push(Pig);
+            dispMsg("You call over to the pig and he seems to listen and follow.");
+            addScore(5);
+         } else {dispMsg("There are no items here!");}
 }
 
 function checkInv() {
-   dispMsg("Bacon = " + hasBacon + ", " + "Ham = " + hasHam);
+   for (var i = 0; i < inventory.length; i++) {
+      dispMsg(inventory[i].name + ", " + inventory[i].desc);
+   }
 }
--- Game Project/locations.js
+++ Game Project/locations.js
@@ -1,4 +1,4 @@
-   // Global Variables
+   // Location Scoring
 var visited0 = false;
 var visited1 = false;
 var visited2 = false;
@@ -10,103 +10,162 @@ var visited7 = false;
 var visited8 = false;
 var visited9 = false;
 
-   // Locations
 function visit0() {
-   dispMsg("You're standing in the middle of a large field.");
    if (! visited0) {
       addScore(0);
       visited0 = true;
-   }
-   
-   availButton();
+   }   
 }
          
 function visit1() {
-   dispMsg("A range of mountains looms in the distance. You feel a soft breeze carrying the smell of bacon from the east.");
    if (! visited1) {
       addScore(5);
       visited1 = true;
-   }
-   
-   availButton();
+   }   
 }
          
 function visit2() {
-   dispMsg("You approach the edge of a cliff. You can't tell how far the drop is and you hear the wind whistling in the canyon.");
    if (! visited2) {
       addScore(5);
       visited2 = true;
-   }
-   
-   availButton();
+   }   
 }
          
 function visit3() {
-   dispMsg("A large river blocks your way. There is an ancient tree nearby. A large object lurks in the west.");
    if (! visited3) {
       addScore(5);
       visited3 = true;
-   }
-   
-   availButton();
+   }  
 }
          
 function visit4() {
-   dispMsg("You've found an old cottage, the door to the east seems to be open.");
    if (! visited4) {
       addScore(5);
       visited4 = true;
-   }
-   
-   availButton();
+   }   
 }
          
 function visit5() {
-   dispMsg("Inside the cottage you find some scattered furniture and bacon grease everywhere. There is a hole in the southern wall.");
    if (! visited5) {
       addScore(5);
       visited5 = true;
-   }
-   
-   availButton();
+   }   
 }
 
 function visit6() {
-   dispMsg("A small path leads from the house to the east...");
    if (! visited6) {
       addScore(5);
       visited6 = true;
-   }
-   
-   availButton();
+   }  
 }
 
 function visit7() {
-   dispMsg("You've come across a gigantic plate of bacon. Control yourself now.");
    if (! visited7) {
       addScore(5);
       visited7 = true;
-   }
-   
-   availButton();
+   }   
 }
 
 function visit8() {
-   dispMsg("No, you are not dreaming. There is a four-foot tall hunk of honey baked ham sitting right in front of you.");
    if (! visited8) {
       addScore(5);
       visited8 = true;
-   }
-   
-   availButton();
+   }   
 }
 
 function visit9() {
-   dispMsg("This is the last room. It's purple on the map. Eat your bacon.");
    if (! visited9) {
       addScore(5);
       visited9 = true;
    }
-   
-   availButton();
 }
+
+
+   // Location Objects
+function Locale () {
+   this.id = "";
+   this.name = "";
+   this.desc = ""; 
+   this.item = "";
+   this.toString = function() {return "[" + this.id + ", " + this.name + ", " + "\"" + this.desc + "\"" + ", " + this.item + "]";}
+}
+            
+var locale0 = new Locale();
+locale0.id = 0;
+locale0.name = "Start";
+locale0.desc = "You're standing in the middle of a large field.";
+locale0.item = false;
+
+
+var locale1 = new Locale();
+locale1.id = 1;
+locale1.name = "Mountains";
+locale1.desc = "A range of mountains looms in the distance. You feel a soft breeze carrying the smell of bacon from the east.";
+locale1.item = false;
+
+
+var locale2 = new Locale();
+locale2.id = 2;
+locale2.name = "Cliff";
+locale2.desc = "You approach the edge of a cliff. You can't tell how far the drop is and you hear the wind whistling in the canyon. There appears to be some silverware near the edge.";
+locale2.item = false;
+
+
+var locale3 = new Locale();
+locale3.id = 3;
+locale3.name = "River";
+locale3.desc = "A large river blocks your way. There is an ancient tree nearby. A large object lurks in the west.";
+locale3.item = false;
+
+
+var locale4 = new Locale();
+locale4.id = 4;
+locale4.name = "Outside Cottage";
+locale4.desc = "You've found an old cottage, the door to the east seems to be open.";
+locale4.item = false;
+
+
+var locale5 = new Locale();
+locale5.id = 5;
+locale5.name = "Cottage";
+locale5.desc = "Inside the cottage you find some scattered furniture and bacon grease everywhere. A scared pig is hiding in a corner. There is a hole in the southern wall.";
+locale5.item = false;
+
+
+var locale6 = new Locale();
+locale6.id = 6;
+locale6.name = "Cottage Path";
+locale6.desc = "A small path leads from the house to the east...";
+locale6.item = false;
+
+var locale7 = new Locale();
+locale7.id = 7;
+locale7.name = "Bacon Plate";
+locale7.desc = "You've come across a gigantic plate of bacon. Control yourself now.";
+locale7.item = "Bacon";
+
+var locale8 = new Locale();
+locale8.id = 8;
+locale8.name = "Ham Plate";
+locale8.desc = "No, you are not dreaming. There is a four-foot tall hunk of honey baked ham sitting right in front of you.";
+locale8.item = "Ham";
+
+var locale9 = new Locale();
+locale9.id = 9;
+locale9.name = "End";
+locale9.desc = "This is the last room. It's purple on the map. Eat your bacon.";
+locale9.item = "Boss";
+
+var locales = [];
+locales[0] = locale0;
+locales[1] = locale1;
+locales[2] = locale2;
+locales[3] = locale3;
+locales[4] = locale4;
+locales[5] = locale5;
+locales[6] = locale6;
+locales[7] = locale7;
+locales[8] = locale8;
+locales[9] = locale9;
+
+
